64df8954b6d502f1f3d9cb0bd8762bc6c1f1f608
sruthyp <36729994+sruthyp@users.noreply.github.com>
2018-07-24 09:48:59 +0530

    Create interval.py

---
+++ interval.py
@@ -0,0 +1,5 @@
+lower_value = int(input("Enter the lower lower_value : "))
+highest_value = int(input("Enter the highest_value : "))
+for i in range(lower_value,highest_value+1):
+  if(i%2 == 0):
+    print(i)
