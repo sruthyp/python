1738f2ff2383fa495fdd9b18c0bbe47b12dc09e7
sruthyp <36729994+sruthyp@users.noreply.github.com>
2018-09-03 15:29:04 +0530

    Create countwords.py

---
+++ countwords.py
@@ -0,0 +1,9 @@
+string=str(input("Enter string:"))
+char=0
+word=1
+for i in string:
+      char=char+1
+      if(i==' '):
+            word=word+1
+print("Number of words in the string:")
+print(word)
