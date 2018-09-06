c8b6071a184f2b07df494af440a449edca3f6d40
sruthyp <36729994+sruthyp@users.noreply.github.com>
2018-09-06 15:37:13 +0530

    Create holworking.py

---
+++ holworking.py
@@ -0,0 +1,7 @@
+a=str(input("print the day:"))
+if(a=='SUNDAY' or a=='Sunday' or a=='sunday'or a=='SATURDAY' or a=='Saturday' or a=='saturday'):
+     print("Holiday")
+else:
+    print("Working day")
+   
+        
