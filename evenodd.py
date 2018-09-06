97ae65e35c17956fa6e5b30a386e0910c0b757f4
sruthyp <36729994+sruthyp@users.noreply.github.com>
2018-09-06 14:58:17 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,6 @@
+a= int(input("Enter a first number: "))
+b= int(input("Enter a second number: "))
+if (a % 2== 0 & b% 2==0):
+   print("{0} is Even".format(a))
+else:
+   print("{0} is Odd".format(a))
